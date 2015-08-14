99744bc6e1daa89303c52f4b1890e6df321d7051
Kyle Pratuch <kyle.pratuch@gmail.com>
2015-08-14 12:15:57 -0700

    add twig templates to display app.

--- app/app.php
+++ app/app.php
@@ -10,9 +10,14 @@
         return $app['twig']->render('repeat_form.html.twig');
     });
 
+    //(Route) Results page with number of repeats.
     $app->get('/results', function() use ($app) {
-        $new = new Repeat;
-    })
+        $new = new RepeatCounter;
+        $word = $_GET['word'];
+        $string = $_GET['string'];
+        $output_count = $new->countRepeats($_GET['word'], $_GET['string']);
+        return $app['twig']->render('repeat_results.html.twig', array('repeats' => $output_count));
+    });
 
-    $
+    return $app;
 ?>

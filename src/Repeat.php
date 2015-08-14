10157eec113aa9b80c7b0d43d608d93d1f0d9738
Kyle Pratuch <kyle.pratuch@gmail.com>
2015-08-14 10:39:19 -0700

    write method for spec 1: pass.

--- src/Repeat.php
+++ src/Repeat.php
@@ -3,7 +3,23 @@
     {
         function countRepeat($input_word, $input_string)
         {
-            
+            //Convert word and string to lower-case.
+            $lc_word = strtolower($input_word);
+            $lc_string = strtolower($input_string);
+
+            //Split lower-cased string into individual words.
+            $split_string = str_split($lc_string);
+
+            //Begin counting matches.
+            $matches = 0;
+
+            foreach ($split_string as $words) {
+                if ($lc_word == $lc_string) {
+                    $matches += 1;
+                }
+            }
+
+            return $matches;
         }
     }
 

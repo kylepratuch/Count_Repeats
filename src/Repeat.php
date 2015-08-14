728fd8b9d2439b54b6910591c48f33b2a4cd80e7
Kyle Pratuch <kyle.pratuch@gmail.com>
2015-08-14 11:38:03 -0700

    edit method to ignore common punctuation: pass spec 9.

--- src/Repeat.php
+++ src/Repeat.php
@@ -10,12 +10,19 @@
             //Explode lower-cased string into individual words.
             $exp_string = explode(" ", $lc_string);
 
+            //Trim common punctuation from word ends in input_string.
+            $trimmed = array();
+            foreach ($exp_string as $exp_word) {
+                $word = trim($exp_word, ".\!\,\?\:\;");
+                array_push($trimmed, $word);
+            }
+
             //Begin counting matches.
             $matches = 0;
 
             //Loop through exploded string to check for matches. Add to match count if found.
-            foreach ($exp_string as $exp_word) {
-                if ($lc_word == $exp_word) {
+            foreach ($trimmed as $trimmed_word) {
+                if ($lc_word == $trimmed_word) {
                     $matches += 1;
                 }
             }
